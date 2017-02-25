ceae0eb617e5f75b6e847171dd909aa2047fca61
pro-continuous-delivery <pro-continuous-delivery@users.noreply.github.com>
2017-02-25 18:40:20 +0100

    Update MessageTest.java

--- src/test/java/hello/MessageTest.java
+++ src/test/java/hello/MessageTest.java
@@ -11,7 +11,7 @@ public class MessageTest {
 
 	@Test
 	public void messageSaysHello() {
-		assertThat(message.sayHello(), containsString("Hello"));
+		assertThat(message.sayHello(), containsString("nikhil"));
 	}
 
 }
